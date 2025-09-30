a3530624df8742f27561d3225c746da3546cbac6
Gyasi Amos Kwadwo <gyasiamoskwadwo@gmail.com>
2025-09-30 18:48:56 +0000

    hosting config

--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://church-web.vercel.app",
+]
 
 ROOT_URLCONF = 'core.urls'
 
